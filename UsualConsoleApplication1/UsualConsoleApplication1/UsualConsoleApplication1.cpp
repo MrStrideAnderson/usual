ec9d9e86fad02e319ca01c3029e418e364d1f922
adore_anderson <adore_anderson@DESKTOP-2TCFNE6>
2016-12-07 21:44:53 +0800

    add 3 space

---
+++ UsualConsoleApplication1/UsualConsoleApplication1/UsualConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+// UsualConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	while (1)
+	{
+
+
+
+
+		printf("hello word");
+		scanf_s(" ");
+	}
+    return 0;
+}
+
