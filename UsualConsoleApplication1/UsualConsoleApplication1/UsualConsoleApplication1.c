fffcf2ac4e1a63e2bef5dc5d02a4976aa6df0021
adore_anderson <adore_anderson@DESKTOP-2TCFNE6>
2017-02-09 11:12:29 +0800

    2017-2-9 11:12:21

--- UsualConsoleApplication1/UsualConsoleApplication1/UsualConsoleApplication1.c
+++ UsualConsoleApplication1/UsualConsoleApplication1/UsualConsoleApplication1.c
@@ -13,17 +13,17 @@
 
 int main()
 {
-	int i = 0,x=0;
-	long dian[10][2] = {0},k=0,b=0;
+	int i = 0, x = 0;
+	long dian[10][2] = { 0 }, k = 0, b = 0;
 	while (scanf_s("%ld %ld", &dian[i][0], &dian[i][1]) == 2 && i < 10)  i++;
 
-	for(int g=0;g < 10;g++)
+	for (int g = 0; g < 10; g++)
 	{
 		printf("%ld %ld\n", dian[i][0], dian[i][1]);
 	}
-	printf("hello word%d",x);
+	printf("hello word%d", x);
 	system("pause");
 	
-    return 0;
+		return 0;
 }
 
